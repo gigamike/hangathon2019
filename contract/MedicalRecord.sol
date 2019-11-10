d11b6f7d5ba212e7978b1fbd2544ea752997a82f
gigamike <michael.gerard.galon@gmail.com>
2019-11-10 23:45:44 +0800

    update smart contract

--- contract/MedicalRecord.sol
+++ contract/MedicalRecord.sol
@@ -8,31 +8,26 @@ contract MedicalRecord {
       string doctor;
       string diagnostics;
       string prescriptions;
+      uint time;
    }
 
    PatientRecord[] public patientRecords;
    uint256 public nextId;
 
-   function setMedicalRecord(address _patientAddress, string memory _hospital, string memory _doctor, string memory _diagnostics, string memory _prescriptions) public{
-    patientRecords.push(PatientRecord(nextId, _patientAddress, _hospital, _doctor, _diagnostics, _prescriptions));
+   function setMedicalRecord(address _patientAddress, string memory _hospital, string memory _doctor, string memory _diagnostics, string memory _prescriptions, uint _time) public{
+    patientRecords.push(PatientRecord(nextId, _patientAddress, _hospital, _doctor, _diagnostics, _prescriptions, _time));
     nextId++;
    }
 
-   function read(uint256 id) view public returns(uint256, address, string memory, string memory, string memory, string memory){
+   function read(uint256 id) view public returns(uint256, address, string memory, string memory, string memory, string memory, uint){
       for(uint256 i=0; i<patientRecords.length; i++){
         if(patientRecords[i].id == id){
-            return(patientRecords[i].id, patientRecords[i].patientAddress, patientRecords[i].hospital, patientRecords[i].doctor, patientRecords[i].diagnostics, patientRecords[i].prescriptions);
+            return(patientRecords[i].id, patientRecords[i].patientAddress, patientRecords[i].hospital, patientRecords[i].doctor, patientRecords[i].diagnostics, patientRecords[i].prescriptions, patientRecords[i].time);
         }
       }
    }
 
-   uint storedData;
-
-    function set(uint x) public {
-        storedData = x;
-    }
-
-    function get() public view returns (uint) {
-        return storedData;
-    }
+   function getMedicalRecordCount() public view returns (uint) {
+    return patientRecords.length;
+  }
 }
